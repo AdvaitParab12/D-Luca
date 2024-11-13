1785c663a40f6ec9a0f789235c1acffcbeac6c11
Advait Parab <advait0012@proton.me>
2024-11-13 08:03:49 +0000

    more customizations added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
 import React from "react";
 import Bellow from "./Bellow";
 function App() {
+  const debates= [
+    {
+      title: "Marvels Vs DC",
+      description: "Marvel is the best",
+    },
+    {
+      title: "React Vs Vue",
+      description: "React is the best",
+    },
+    {
+      title: "Java Vs Kotlin",
+      description: "Kotlin is the best",
+    },
+    {
+      title: "Coke Vs Pepsi",
+      description: "Coke is the best",
+    },
+  ];
   return (
     <div className="flex items-center justify-center h-screen marker:text-red-500">
-      <Bellow />
+      <Bellow data={debates}  filterByLetter="R" isOpen={false} />
     </div>
   );
 }
--- src/Bellow.jsx
+++ src/Bellow.jsx
@@ -1,42 +1,19 @@
 import React from "react";
 import Accordion from "./Accordion";
 
-function Bellow() {
-  const data = [
-    {
-      title: "Marvels Vs DC",
-      description: "Marvel is the best",
-    },
-    {
-      title: "React Vs Vue",
-      description: "React is the best",
-    },
-    {
-      title: "Java Vs Kotlin",
-      description: "Kotlin is the best",
-    },
-    {
-      title: "Coke Vs Pepsi",
-      description: "Coke is the best",
-    },
-  ];
-
-  const Accordions = [];
-  for (let i = 0; i < data.length; i++) {
-    Accordions.push(
-      <Accordion
-        key={crypto.randomUUID()}
-        title={data[i].title}
-        description={data[i].description}
-        isOpen={i === 0}
-      />
-    );
-  }
-  console.log(Accordions);
-  
+function Bellow({ data = [],filterByLetter="", isOpen=false }) {
   return (
-    <div className="border-2 border-zinc-600 rounded-lg p-4">
-      {Accordions}
+    <div className="border-2 border-zinc-600 rounded-lg">
+      {data
+        .filter((d) => d.title.toLowerCase().includes(filterByLetter.toLowerCase()))
+        .map((d) => (
+          <Accordion
+            key={crypto.randomUUID()}
+            title={d.title}
+            description={d.description}
+            isOpen={isOpen}
+          />
+        ))}
     </div>
   );
 }
